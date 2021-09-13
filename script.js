00af2b2d056d47d7b3f0db6c10caadae1e4bfbb1
ManhimYeung <14059614@students.southwales.ac.uk>
2021-09-13 16:28:16 +0100

    Added a function to find the thief

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const message2 = ["is a", "is not a"];
 const message3 = ["programmer", "student", "thief", "begger", "cleaner"];
 
 
-const mixedMessageGenerator = () => {
+const mixedMessageGenerator = (array) => {
     let randMsg1 = Math.floor(Math.random() * message1.length);
     let randMsg2 = Math.floor(Math.random() * message2.length);
     let randMsg3 = Math.floor(Math.random() * message3.length);
@@ -12,6 +12,18 @@ const mixedMessageGenerator = () => {
     generatedMessage.push(message1[randMsg1], message2[randMsg2], message3[randMsg3]);
     let spaces = generatedMessage.join(" ");
     console.log(spaces);
+    
+    return generatedMessage;
 }
 
-mixedMessageGenerator();
+const findIllegal = (array) => {
+    if (array[1] === "is a" && array[2] === "thief") {
+        console.log("Thief caught!");
+        return true;
+    } else {
+        console.log("Thief not caught!");
+        return false;
+    }
+}
+
+findIllegal(mixedMessageGenerator());
